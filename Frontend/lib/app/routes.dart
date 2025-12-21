8be210d5a6b004d333a3d93e469bdd774d79b88f
Teja Varshith <learnatyour@gmail.com>
2025-12-21 16:24:41 +0530

    carrer aplpictaion

--- Frontend/lib/app/routes.dart
+++ Frontend/lib/app/routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:unisync/features/auth/view/fill_tank.dart';
 import 'package:unisync/features/auth/view/login_screen.dart';
-import 'package:unisync/features/homescreen/view/home_screen.dart';
+import 'package:unisync/features/uni-2/carrer_main_screen.dart';
 
 final loggedOutRoutes = RouteMap(
   routes: {
@@ -19,6 +19,7 @@ final completeProfileRoutes = RouteMap(
 
 final loggedInRoutes = RouteMap(
   routes: {
-    '/': (_) => MaterialPage(child: HomeScreen())
+    // '/': (_) => MaterialPage(child: HomeScreen()),
+    "/": (_) => MaterialPage(child: CareerScreen()),
   }
 );
